7df53e1fdb683cb924bf703608c294c1dc6d69fd
soufiane <jabersoufiane605@gmail.com>
2024-02-19 22:47:18 +0100

    fix width

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,8 +1,8 @@
-function Button({ children, onClick }) {
+function Button({ children, onClick, more = "" }) {
   return (
     <button
       onClick={onClick}
-      className="bg-stone-100 hover:bg-stone-200  transition-all px-6 py-2.5 rounded-md">
+      className={` bg-stone-100 ${more} hover:bg-stone-200  transition-all px-6 py-2.5 rounded-md focus:outline-stone-400 `}>
       {children}
     </button>
   );
