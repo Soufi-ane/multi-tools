65266761506e1aeceae7441fa19bea8c6ffee9f2
soufiane <jabersoufiane605@gmail.com>
2024-02-19 22:43:03 +0100

    fix position

--- src/ui/Container.jsx
+++ src/ui/Container.jsx
@@ -1,7 +1,7 @@
 function Container({ children, className }) {
   return (
     <div
-      className={`bg-white w-11/12 md:w-9/12 h-[90%] mx-auto mt-28 md:mt-10 rounded-md relative flex flex-col gap-16 pb-6 pt-16 sm:py-16 px-32 items-center ${className}`}>
+      className={`bg-white w-11/12 md:w-9/12  mx-auto pt-16  rounded-md relative flex flex-col gap-16 pb-6  sm:py-16 px-32 items-center border-2 border-stone-200 ${className}`}>
       {children}
     </div>
   );
